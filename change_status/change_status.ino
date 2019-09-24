9f39acfdd36f1b627a10450686528d5c15088476
jing <joyceli.yijing@gmail.com>
2019-09-24 12:52:09 +0800

    Create change_status.ino

---
+++ change_status/change_status.ino
@@ -0,0 +1,30 @@
+
+int i = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, INPUT);
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+  change();
+  i_status();
+ 
+}
+
+// when press the button , change i = 0,1,2 
+void change(){
+  if(HIGH == digitalRead(2) ){
+       i = (i+1)%3;          
+  }
+  delay(200);
+}
+
+// get i status
+int i_status(){
+  Serial.println(i); 
+  return i;  
+}
