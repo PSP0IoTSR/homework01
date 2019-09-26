44111afd40856a9fc07205f8e951baa204deb50e
toto1222 <37579029+toto1222@users.noreply.github.com>
2019-09-26 21:26:55 +0800

    Add files via upload

--- library/led.cpp
+++ library/led.cpp
@@ -5,91 +5,96 @@ led::led()
 {
     //ctor
 }
-led::led(int a){
-length  = a ;
+led::led(int a,int pinIn[8]){
+length  = a
+for(int i = 0 ; i < 8 ; i++)
+{
+    pin[i] = pinIn[i];
+}
+
 switch(length){
     case 1:
-        digitalWrite(2,HIGH);
-        digitalWrite(3,LOW);
-        digitalWrite(4,LOW);
-        digitalWrite(5,LOW);
-        digitalWrite(6,LOW);
-        digitalWrite(7,LOW);
-        digitalWrite(8,LOW);
-        digitalWrite(9,LOW);
+        digitalWrite(pin[0],HIGH);
+        digitalWrite(pin[1],LOW);
+        digitalWrite(pin[2],LOW);
+        digitalWrite(pin[3],LOW);
+        digitalWrite(pin[4],LOW);
+        digitalWrite(pin[5],LOW);
+        digitalWrite(pin[6],LOW);
+        digitalWrite(pin[7],LOW);
     case 2:
-        digitalWrite(2,HIGH);
-        digitalWrite(3,HIGH);
-        digitalWrite(4,LOW);
-        digitalWrite(5,LOW);
-        digitalWrite(6,LOW);
-        digitalWrite(7,LOW);
-        digitalWrite(8,LOW);
-        digitalWrite(9,LOW);
+        digitalWrite(pin[0],HIGH);
+        digitalWrite(pin[1],HIGH);
+        digitalWrite(pin[2],LOW);
+        digitalWrite(pin[3],LOW);
+        digitalWrite(pin[4],LOW);
+        digitalWrite(pin[5],LOW);
+        digitalWrite(pin[6],LOW);
+        digitalWrite(pin[7],LOW);
     case 3:
-        digitalWrite(2,HIGH);
-        digitalWrite(3,HIGH);
-        digitalWrite(4,HIGH);
-        digitalWrite(5,LOW);
-        digitalWrite(6,LOW);
-        digitalWrite(7,LOW);
-        digitalWrite(8,LOW);
-        digitalWrite(9,LOW);
+        digitalWrite(pin[0],HIGH);
+        digitalWrite(pin[1],HIGH);
+        digitalWrite(pin[2],HIGH);
+        digitalWrite(pin[3],LOW);
+        digitalWrite(pin[4],LOW);
+        digitalWrite(pin[5],LOW);
+        digitalWrite(pin[6],LOW);
+        digitalWrite(pin[7],LOW);
     case 4:
-        digitalWrite(2,HIGH);
-        digitalWrite(3,HIGH);
-        digitalWrite(4,HIGH);
-        digitalWrite(5,HIGH);
-        digitalWrite(6,LOW);
-        digitalWrite(7,LOW);
-        digitalWrite(8,LOW);
-        digitalWrite(9,LOW);
+        digitalWrite(pin[0],HIGH);
+        digitalWrite(pin[1],HIGH);
+        digitalWrite(pin[2],HIGH);
+        digitalWrite(pin[3],HIGH);
+        digitalWrite(pin[4],LOW);
+        digitalWrite(pin[5],LOW);
+        digitalWrite(pin[6],LOW);
+        digitalWrite(pin[7],LOW);
     case 5:
-        digitalWrite(2,HIGH);
-        digitalWrite(3,HIGH);
-        digitalWrite(4,HIGH);
-        digitalWrite(5,HIGH);
-        digitalWrite(6,HIGH);
-        digitalWrite(7,LOW);
-        digitalWrite(8,LOW);
-        digitalWrite(9,LOW);
+        digitalWrite(pin[0],HIGH);
+        digitalWrite(pin[1],HIGH);
+        digitalWrite(pin[2],HIGH);
+        digitalWrite(pin[3],HIGH);
+        digitalWrite(pin[4],HIGH);
+        digitalWrite(pin[5],LOW);
+        digitalWrite(pin[6],LOW);
+        digitalWrite(pin[7],LOW);
     case 6:
 
-        digitalWrite(2,HIGH);
-        digitalWrite(3,HIGH);
-        digitalWrite(4,HIGH);
-        digitalWrite(5,HIGH);
-        digitalWrite(6,HIGH);
-        digitalWrite(7,HIGH);
-        digitalWrite(8,LOW);
-        digitalWrite(9,LOW);
+        digitalWrite(pin[0],HIGH);
+        digitalWrite(pin[1],HIGH);
+        digitalWrite(pin[2],HIGH);
+        digitalWrite(pin[3],HIGH);
+        digitalWrite(pin[4],HIGH);
+        digitalWrite(pin[5],HIGH);
+        digitalWrite(pin[6],LOW);
+        digitalWrite(pin[7],LOW);
     case 7:
-        digitalWrite(2,HIGH);
-        digitalWrite(3,HIGH);
-        digitalWrite(4,HIGH);
-        digitalWrite(5,HIGH);
-        digitalWrite(6,HIGH);
-        digitalWrite(7,HIGH);
-        digitalWrite(8,HIGH);
-        digitalWrite(9,LOW);
+        digitalWrite(pin[0],HIGH);
+        digitalWrite(pin[1],HIGH);
+        digitalWrite(pin[2],HIGH);
+        digitalWrite(pin[3],HIGH);
+        digitalWrite(pin[4],HIGH);
+        digitalWrite(pin[5],HIGH);
+        digitalWrite(pin[6],HIGH);
+        digitalWrite(pin[7],LOW);
     case 8:
-        digitalWrite(2,HIGH);
-        digitalWrite(3,HIGH);
-        digitalWrite(4,HIGH);
-        digitalWrite(5,HIGH);
-        digitalWrite(6,HIGH);
-        digitalWrite(7,HIGH);
-        digitalWrite(8,HIGH);
-        digitalWrite(9,HIGH);
+        digitalWrite(pin[0],HIGH);
+        digitalWrite(pin[1],HIGH);
+        digitalWrite(pin[2],HIGH);
+        digitalWrite(pin[3],HIGH);
+        digitalWrite(pin[4],HIGH);
+        digitalWrite(pin[5],HIGH);
+        digitalWrite(pin[6],HIGH);
+        digitalWrite(pin[7],HIGH);
       default:
-        digitalWrite(2,LOW);
-        digitalWrite(3,LOW);
-        digitalWrite(4,LOW);
-        digitalWrite(5,LOW);
-        digitalWrite(6,LOW);
-        digitalWrite(7,LOW);
-        digitalWrite(8,LOW);
-        digitalWrite(9,LOW);
+        digitalWrite(pin[0],LOW);
+        digitalWrite(pin[1],LOW);
+        digitalWrite(pin[2],LOW);
+        digitalWrite(pin[3],LOW);
+        digitalWrite(pin[4],LOW);
+        digitalWrite(pin[5],LOW);
+        digitalWrite(pin[6],LOW);
+        digitalWrite(pin[7],LOW);
 
   }
 }
